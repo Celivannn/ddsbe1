5bcd16b044c21e44af869024c7108fcfd9fdcea1
Celivannn <sinadjanivan@gmail.com>
2025-04-12 00:14:11 +0800

    deployment

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,22 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/users', ['uses' => 'UserController@getUsers']);
+$router->group(['prefix' => 'api'], function () use ($router) {
 
     $router->get('/users', 'UserController@index');//get all users records
-    $router->post('/users', 'UserController@addUser');//create new user record
+    $router->post('/users', 'UserController@add');//create new user record
     $router->get('/users/{id}', 'UserController@show');//get user by id
     $router->put('/users/{id}', 'UserController@update');//update user record
     $router->patch('/users/{id}', 'UserController@update');//update user record
     $router->delete('/users/{id}', 'UserController@delete');//delete record
+});
 
-    //user job routes
-    $router->get('/usersjob', 'UserJobController@index'); // Get all jobs
-    $router->get('/usersjob/{id}', 'UserJobController@show'); // Get job by ID
-    
+$router->get('/users', 'UserController@index');//get all users records
+$router->post('/users', 'UserController@add');//create new user record
+$router->get('/users/{id}', 'UserController@show');//get user by id
+$router->put('/users/{id}', 'UserController@update');//update user record
+$router->patch('/users/{id}', 'UserController@update');//update user record
+$router->delete('/users/{id}', 'UserController@delete');//delete record
+
+$router->get('/usersjob', 'UserJobController@index');
+$router->get('/userjob/{id}', 'UserJobController@show'); // get user by id
